d9b54a42400b442df688654df8d426250cbecf75
Madan.patakota <madan.patakota@gmail.com>
2025-12-25 11:11:05 +0530

    services

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,8 +1,22 @@
+
+using Introduction.Interfaces;
+using Introduction.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ISingletonCoffee, CoffeeService>();
+
+builder.Services.AddScoped<IScopedCoffee, CoffeeService>();
+
+
+builder.Services.AddTransient<ITransientCoffee, CoffeeService>();
+
+
+
 
 var app = builder.Build();
 
---
+++ Introduction/Services/CoffeeService.cs
@@ -0,0 +1,18 @@
+﻿
+using Introduction.Interfaces;
+
+namespace Introduction.Services
+{
+    public class CoffeeService : ISingletonCoffee , ITransientCoffee , IScopedCoffee
+    {
+        private readonly string _coffeeId;
+        public CoffeeService()
+        {
+            _coffeeId = Guid.NewGuid().ToString();
+        }
+        public string GetCoffeeId()
+        {
+            return _coffeeId;
+        }
+    }
+}
